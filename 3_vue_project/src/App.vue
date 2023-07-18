8e92460248043aa8d16572b2e5fbc7cfc6db1ad3
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-07-17 21:31:05 -0300

    Importando componentes.

--- 3_vue_project/src/App.vue
+++ 3_vue_project/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1>Olá Mundo!</h1>
+  <PrimeiroComponente />
 </template>
 
 <script>
+import PrimeiroComponente from './components/PrimeiroComponente.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    PrimeiroComponente
+  }
 }
 </script>
