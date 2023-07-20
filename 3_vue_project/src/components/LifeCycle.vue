4105e022325d214b9d610eb2d7ae0a2f7b11043c
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-07-20 00:00:04 -0300

    Melhorando hooks

---
+++ 3_vue_project/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu nome é: {{ nome }}.</h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return{
+            nome: 'Sei lá!!'
+        }
+    },
+    created() {
+        setTimeout(() => {
+            this.nome = 'Matias'
+        }, 2000);
+    },
+    mounted() {
+        setTimeout(() => {
+            this.nome = 'Pedro'
+        }, 1000);
+    }    
+}
+</script>
--- 3_vue_project/src/components/PrimeiroComponente.vue
+++ 3_vue_project/src/components/PrimeiroComponente.vue
@@ -14,6 +14,5 @@ export default {
             profissao: "Programador"
         }
     }
-
 }
 </script>
