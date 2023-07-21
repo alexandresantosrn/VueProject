3c6bfe00a5014b05aec346ecea612d4d6ada314f
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-07-21 13:14:15 -0300

    Implementando diretivas condicionais: v-if, v-else e v-show.

--- 3_vue_project/src/components/Information.vue
+++ 3_vue_project/src/components/Information.vue
@@ -1,17 +1,27 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades!</p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>JavaScript</li>
             <li>PHP</li>
             <li>Python</li>
-        </ul>      
+        </ul>  
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}.</p>
+        <p v-if=" 4 > 2">Testando.</p>    
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Information'
+    name: 'Information',
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: true,
+            email: 'alex@google.com'
+        }
+    } 
 }
 </script>
