641dee5e5cb40a85b1a2f10a6a00c990cb6cf1bc
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-07-20 23:17:57 -0300

    Implementando hierquia de componentes.

---
+++ 3_vue_project/src/components/Pessoa.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Esta é a descrição da pessoa: {{ nome }}.</h2>         
+    </div>   
+</template>
+
+<script>
+
+export default {
+    name: 'Pessoa',
+    
+    data() {
+        return {
+            nome: 'Alexandre'
+        }
+    }
+}
+</script>
