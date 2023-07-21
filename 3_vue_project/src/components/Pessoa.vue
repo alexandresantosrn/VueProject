22d690041256f257bf9e9f460f3cf4a408420ecd
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-07-20 23:50:49 -0300

    Corrigindo bugs.

--- 3_vue_project/src/components/Pessoa.vue
+++ 3_vue_project/src/components/Pessoa.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
-        <h2>Esta é a descrição da pessoa: {{ nome }}.</h2>         
+        <h2>Esta é a descrição da pessoa: {{ nome }}.</h2>  
+        <Information />               
     </div>   
 </template>
 
 <script>
+import Information from './Information.vue';
 
 export default {
     name: 'Pessoa',
-    
+    components: {
+        Information
+    },
     data() {
         return {
             nome: 'Alexandre'
