262765fd771eb40acd29361ca6b46a15036ccc67
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-07-24 21:49:47 -0300

    Usando CSS Scoped.

--- 3_vue_project/src/App.vue
+++ 3_vue_project/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Header />
     <PrimeiroComponente />
     <LifeCycle />
     <Pessoa />
@@ -10,13 +11,15 @@
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
 import LifeCycle from './components/LifeCycle.vue';
 import Pessoa from './components/Pessoa.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
     PrimeiroComponente,
     LifeCycle,
-    Pessoa
+    Pessoa,
+    Header
   }
 }
 </script>
