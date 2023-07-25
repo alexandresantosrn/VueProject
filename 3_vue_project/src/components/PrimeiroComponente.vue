d802be12887995c94c7b8797fdbb5ba2f2168407
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-07-25 17:06:26 -0300

    Implementando reutilização dos componentes.

--- 3_vue_project/src/components/PrimeiroComponente.vue
+++ 3_vue_project/src/components/PrimeiroComponente.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
+        <Reutilizacao />
         <h1> Olá mundo! </h1>
         <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}.</p>
         <MultiplosEventos />
+        <Reutilizacao />
     </div>
 </template>
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue';
+import Reutilizacao from './Reutilizacao.vue';
 
 export default {
     name: 'PrimeiroComponente',
@@ -18,7 +21,8 @@ export default {
         }
     },
     components: {
-    MultiplosEventos
+        MultiplosEventos,
+        Reutilizacao
     }
 }
 </script>
